8e42a75c834fa92770d5688d24fa8885881d9a6d
Zuhayr Amin <zuhayr@Zuhayrs-MacBook-Pro.local>
2023-06-21 11:15:42 +0500

    Created initial Author Routes
    Added funtionality to add an author and search for the authors already in the database

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,19 @@ const router = express.Router()
 const Author = require('../models/author')
 
 // All Author route
-router.get('/', (req, res) => {
-    res.render('authors/index')
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query})
+    } catch {
+        res.redirect('/')
+    }
 })
 
 // New Author Route
@@ -13,21 +24,30 @@ router.get('/new', (req, res) => {
 })
 
 // Create Author Route
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const author = new Author({
         name: req.body.name
     })
-
-    author.save((error, newAuthor) => {
-        if(error){
-            res.render('authors/new', {
+    
+    try {
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+    } catch {
+        res.render('authors/new', {
                 author: author,
                 errorMessage: "Error creating Author"
-            })
-        }
-        // res.redirect(`authors/${newAuthor.id}`)
-        res.redirect(`authors`)
-    })
+        })
+    }
+    // author.save().then(item => {
+    //     // res.redirect(`authors/${newAuthor.id}`)
+    //     res.redirect('authors')
+    // }).catch(err => {
+    //     res.render('authors/new', {
+    //         author: author,
+    //         errorMessage: "Error creating Author"
+    //     })
+    // })
 })
 
 module.exports = router
