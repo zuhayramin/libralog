eea16f390d0ecd407196043802b44d5b1c4c9922
Zuhayr Amin <zuhayr@Zuhayrs-MacBook-Pro.local>
2023-06-22 11:07:09 +0500

    Adding/Searching/Filtering Books
    You can now add books using a form. After submitting, all books and their covers can be seen from the Books page and home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     // This will render the index.ejs file
-    res.render('index')
+    let books
+    try {
+        books = await Book.find().sort({createAt: 'desc'}).limit(10).exec()
+    } catch {
+        books = []
+    }
+    res.render('index', {books: books})
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -21,8 +21,8 @@ db.once('open', () => console.log("Connected to Mongoose"))
 
 // This is getting our index.js file from the 'routes' folder
 const indexRouter = require('./routes/index')
-
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 // The view engine facilitates the combining of data from the server with templates, before being sent to the client
 app.set('view engine', 'ejs')
@@ -36,6 +36,8 @@ app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
+
 // This will pull the PORT from the environment variable. When we deploy, the server will tell us which port it is listening to, not us
 // For development, we will just use the PORT 3000
 app.listen(process.env.PORT || 3000)
